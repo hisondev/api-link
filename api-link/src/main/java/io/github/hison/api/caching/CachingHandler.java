c1cd11b5ebf75371caddd7fc3b6413743f6df8c2
hisondev <64723261+hisondev@users.noreply.github.com>
2025-02-11 12:36:48 +0900

    add setRegistry method on CachingHandler

--- api-link/src/main/java/io/github/hison/api/caching/CachingHandler.java
+++ api-link/src/main/java/io/github/hison/api/caching/CachingHandler.java
@@ -3,12 +3,15 @@ package io.github.hison.api.caching;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistration;
 
 /** 
  * @author Hani son
- * @version 1.0.1
+ * @version 1.0.2
  */
 public interface CachingHandler {
+    public void setRegistry(WebSocketHandlerRegistration  registry);
+
     public void addSession(CopyOnWriteArrayList<WebSocketSession> sessions, WebSocketSession session);
     
     public void removeSession(CopyOnWriteArrayList<WebSocketSession> sessions, WebSocketSession session);
