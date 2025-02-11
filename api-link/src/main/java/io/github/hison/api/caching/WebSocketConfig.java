dae016e3e0a04a84a76d9694781742e47ddf3d6c
hisondev <64723261+hisondev@users.noreply.github.com>
2025-02-11 12:38:19 +0900

    add Configation, EnableWebSocket annotation and setRegistry logic

--- api-link/src/main/java/io/github/hison/api/caching/WebSocketConfig.java
+++ api-link/src/main/java/io/github/hison/api/caching/WebSocketConfig.java
@@ -1,17 +1,27 @@
 package io.github.hison.api.caching;
 
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 
 /** 
  * @author Hani son
- * @version 1.0.1
+ * @version 1.0.2
  */
+@Configuration
+@EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
     private final CachingWebSocketSessionManager sessionManager = CachingWebSocketSessionManager.getInstance();
+    private final CachingHandler handler;
+
+    public WebSocketConfig() {
+        this.handler = CachingHandlerFactory.getHandler();
+    }
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new WebSocketHandler(sessionManager), sessionManager.getEndPoint());
+    public void registerWebSocketHandlers(@NonNull WebSocketHandlerRegistry registry) {
+        handler.setRegistry(registry.addHandler(new WebSocketHandler(sessionManager), sessionManager.getEndPoint()));
     }
 }
